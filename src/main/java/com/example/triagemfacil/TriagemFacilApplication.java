2d5040348a21e4f03a0707e3371aa0f71e892ca4
Danilo Agostinho <daniloagostinhosilva@gmail.com>
2023-12-04 21:14:19 -0300

    second commit

---
+++ src/main/java/com/example/triagemfacil/TriagemFacilApplication.java
@@ -0,0 +1,13 @@
+package com.example.triagemfacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriagemFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriagemFacilApplication.class, args);
+	}
+
+}
